2dd881b3b2e34d91de9635ca4fc42c7f4f1ded93
Adv0cad0 <lubasha127@gmail.com>
2025-11-05 22:39:56 +0300

    fg

--- lesson1/chet.js
+++ lesson1/chet.js
@@ -14,10 +14,26 @@
 //     console.log(i)
 //     i++
 // }
-
-while (true) {
-    let number = prompt("Введите число")
-    if (number > 100) {
+//
+// while (true) {
+//     let number = prompt("Введите число")
+//     if (number > 100) {
+//         break
+//     }
+// }
+const number = +prompt('Введите число между 0 и 3', '');
+switch (number) {
+    case 0:
+        alert("0");
         break
-    }
+    case 1:
+        alert("1");
+        break
+    case 2:
+    case 3:
+        alert("2, 3");
+        break
+    default:
+        alert("хуйня")
+
 }
