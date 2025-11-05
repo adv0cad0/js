42e582aa3ea4a1fb684e6841da4a5275e077b2ab
Adv0cad0 <lubasha127@gmail.com>
2025-11-05 22:20:53 +0300

    fg

---
+++ lesson1/chet.js
@@ -0,0 +1,23 @@
+// for (let i = 2; i<=10; i++) {
+//
+//     if (i%2 === 0) {
+//         console.log(i)
+//
+//     } else {
+//
+//         console.log(`Нечетное число ${i}`)
+//     }
+//
+// }
+// let i = 0;
+// while (i < 3) {
+//     console.log(i)
+//     i++
+// }
+
+while (true) {
+    let number = prompt("Введите число")
+    if (number > 100) {
+        break
+    }
+}
