4c412e80b55ab81098ab7d6bef54be8876c539f8
Adv0cad0 <lubasha127@gmail.com>
2025-11-06 22:03:42 +0300

    hdf

--- lesson1/chet.js
+++ lesson1/chet.js
@@ -21,19 +21,83 @@
 //         break
 //     }
 // }
-const number = +prompt('Введите число между 0 и 3', '');
-switch (number) {
-    case 0:
-        alert("0");
-        break
-    case 1:
-        alert("1");
-        break
-    case 2:
-    case 3:
-        alert("2, 3");
-        break
-    default:
-        alert("хуйня")
+// const number = +prompt('Введите число между 0 и 3', '');
+// switch (number) {
+//     case 0:
+//         alert("0");
+//         break
+//     case 1:
+//         alert("1");
+//         break
+//     case 2:
+//     case 3:
+//         alert("2, 3");
+//         break
+//     default:
+//         alert("хуйня")
+//
+// }
+//
+// function checkAge(age=18) {
+//     let result = (age > 18) ? true : confirm('Родители разрешили?');
+//     return result
+// }
+//
+//
+// if (checkAge()) {
+//     alert("доступно")
+// } else {
+//     alert("нет")
+//
+// }
+//
+// function min(a, b) {
+//     if (a - b < 0) {
+//         return a;
+//     } else {
+//         return b
+//     }
+// }
+//
+// console.log(min(2, 2))
 
-}
+// function pow(x, n) {
+//
+//     return Math.pow(x, n)
+//
+// }
+//
+// console.log(pow(2, 3));
+//
+// function multiply(a, b) {
+//     return a * b;
+// }
+//
+// console.log(multiply(5, 5))
+// function isMaxim(a) {
+//     if (a === "Максим") {
+//         return "Гей";
+//     }
+//     if (!a) {
+//         return "пиши, ёпт";
+//     }
+//     if (a !== "Максим") {
+//         return "Отлично"
+//     }
+// }
+//
+// console.log(isMaxim("Максим"))
+// let hp = 100;
+//
+// function attack(a = 10, b = 1) {
+//
+//     hp = hp - a * b
+//     if (hp <= 0) {
+//         return "Ты помер"
+//     }
+//     return `Осталось ${hp}`
+// }
+//
+// console.log(attack(20, 2))
+// console.log(attack(50))
+// console.log(attack(50))
