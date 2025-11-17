be6eb20be6bf5f1e934dfd6e9adaa9b9be6e1d03
HOME-PC\Max <gmd29999@yandex.ru>
2025-11-17 23:21:04 +0300

    obj

---
+++ lesson1/objects/1.js
@@ -0,0 +1,45 @@
+// const user = {
+//     name: "John",
+//     surname: "Smith",
+// };
+// user.name = "Pete";
+// delete user.name;
+//
+// user.age = 30;
+//
+// console.log("name" in user)
+//
+// let schedule = {};
+//
+// console.log (isEmpty(schedule) ); // true
+//
+// schedule["8:30"] = "get up";
+//
+// console.log (isEmpty(schedule) ); // (false)
+//
+// function isEmpty (obj) {
+//     for (let key in obj){
+//         return false
+//     }
+//     return true
+// }
+//
+// let salaries = {
+//     John: 100,
+//     Ann: 160,
+//     Pete: 130
+//
+// }
+//
+//
+// function sum(sal){
+//     let sumSal = 0
+//     for (let name in sal){
+//         sumSal = sumSal + sal[name]
+//     }
+//
+//     return sumSal
+// }
+//
+// const depSal = sum(salaries)
+// console.log(depSal)
